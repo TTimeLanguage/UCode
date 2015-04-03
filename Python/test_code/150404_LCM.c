bda5ed55a5fc1f57b93799a889758bb4d225226d
isac322 <isac322@naver.com>
2015-04-04 00:34:59 +0900

    add test code
    calculating lcm

---
+++ Python/test_code/150404_LCM.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int gcd(int a, int b) {
+	int tmp;
+	while (b) {
+		tmp = a%b;
+		a = b;
+		b = tmp;
+	}
+	return a;
+}
+
+int gcd2(int a, int b) {
+	if (!b) return a;
+	return gcd2(b, a%b);
+}
+
+int main() {
+	int n, a, b;
+	scanf("%d", &n);
+	while (n--) {
+		scanf("%d %d", &a, &b);
+		printf("%d\n", (a*b) / gcd(a, b));
+		printf("%d\n", (a*b) / gcd2(a, b));
+	}
+}
